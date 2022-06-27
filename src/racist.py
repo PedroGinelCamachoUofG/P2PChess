3927f93fb24252835630d08778e008067f0c8f40
Firstname Surname <2563638G@student.gla.ac.uk>
2022-06-27 10:48:54 +0200

    -Deleted "joke" file from Alberto

--- src/racist.py
+++
@@ -1,8 +0,0 @@
-from itertools import permutations
-coordinates = 0
-lst = [1,2]
-combinations = list(permutations(lst))
-for x in range(len(combinations)):
-
-    print(combinations[x])
-positions = []
